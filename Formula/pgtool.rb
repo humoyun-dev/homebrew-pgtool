0e2688bd196f27d217ddee8b32e9bd2b54d4916c
humoyun-dev <humoyunbektursunniyazov@gmail.com>
2025-11-27 23:45:31 +0500

    Bump pgtool to v0.2.0

--- Formula/pgtool.rb
+++ Formula/pgtool.rb
@@ -1,8 +1,8 @@
 class Pgtool < Formula
   desc "CLI to install and manage PostgreSQL users & databases"
   homepage "https://github.com/humoyun-dev/pgtool"
-  url "https://github.com/humoyun-dev/pgtool/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "dbd1ec45862633288e1c7f9353e3bba8971f4a498ff746af73d595d6a8645956"
+  url "https://github.com/humoyun-dev/pgtool/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "ca6d8035f9e03c60feae2ac3e23f0eac93da9e4d49c3f718c902b1791b6bca19"
   license "MIT"
 
   depends_on "go" => :build
