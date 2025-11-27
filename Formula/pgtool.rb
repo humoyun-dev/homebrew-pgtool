f5442422ddc94df3e01488658ac73b6b6ca7319c
humoyun-dev <humoyunbektursunniyazov@gmail.com>
2025-11-27 11:20:26 +0500

    Add pgtool Homebrew formula

--- Formula/pgtool.rb
+++ Formula/pgtool.rb
@@ -2,7 +2,7 @@ class Pgtool < Formula
   desc "CLI to install and manage PostgreSQL users & databases"
   homepage "https://github.com/humoyun-dev/pgtool"
   url "https://github.com/humoyun-dev/pgtool/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "a4bf0db878a9cc7a17816886148a99ec1595f3626da7f967aaa969eb063552f4"
+  sha256 "c3d392a61fc028e557b2d2704748c2ee93b1dee07b618741595fbab9929aebe6"
   license "MIT"
 
   depends_on "go" => :build
